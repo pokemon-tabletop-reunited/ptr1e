bbf8ccccba1bffdc9d7fd972806f6d1fe8fbe6d1
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2026-04-12 09:04:40 -0600

    Make ptubuilder id 16 characters

--- utils/packs.mjs
+++ utils/packs.mjs
@@ -82,7 +82,7 @@ function cleanPackEntry(data, { clearSourceId=true, ownership=0 }={}) {
   }
   delete data.flags?.importSource;
   delete data.flags?.exportSource;
-  if ( data._stats?.lastModifiedBy ) data._stats.lastModifiedBy = "ptubuilder00000000";
+  if ( data._stats?.lastModifiedBy ) data._stats.lastModifiedBy = "ptubuilder000000";
 
   // Remove empty entries in flags
   if ( !data.flags ) data.flags = {};
